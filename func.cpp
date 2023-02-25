df81ae08387cd88941858ff67b06e848320814d2
tsumugi <85925233+tsumugiw@users.noreply.github.com>
2023-02-26 08:10:32 +0900

    aa

--- func.cpp
+++ func.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <kv/interval.hpp>
+#include <kv/dd.hpp>
+#include <kv/rdd.hpp>
+#include <kv/mpfr.hpp>
 
-typedef kv::interval<double>	itv;
+// #define MPFR_FLG
+// #define DD_FLG
+
+#ifdef MPFR_FLG
+    typedef kv::interval<kv::mpfr<106>> itv;
+#ifdef DD_FLG
+    typedef kv::interval<kv::dd> itv;
+#else
+    typedef kv::interval<double> itv;
+#endif
 
 template <class T>
 T func(T x)
@@ -11,11 +23,10 @@ T func(T x)
      return (T)1.0 / exp((T)0.5 * x / pi);
 }
 
-
 int main(void)
 {
     itv x,res;
-    x = "0.1";
+    x = 0.1;
     res = func(x);
     std::cout.precision(17);
     std::cout << "  x  = " << x << '\n';
