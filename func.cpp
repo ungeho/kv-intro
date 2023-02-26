f77b4fd944e71b4134f3af77672e38583f7b622c
ungeho <ungeho>
2023-02-27 01:28:35 +0900

    aaa

--- func.cpp
+++ func.cpp
@@ -24,13 +24,13 @@ T func(T x)
 {
     itv pi;
     pi = kv::constants<itv>::pi();
-     return (T)1.0 / exp((T)0.5 * x / pi);
+     return (T)"1.0" / exp((T)"0.5" * x / pi);
 }
 
 int main(void)
 {
     itv x,res;
-    x = 0.1;
+    x = "0.1";
     res = func(x);
     std::cout.precision(17);
     std::cout << "  x  = " << x << '\n';
