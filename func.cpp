861fc02b7021f3ecea6396750e495c90b6c47bb0
ungeho <ungeho>
2023-03-01 00:53:09 +0900

    aaa

--- func.cpp
+++ func.cpp
@@ -12,15 +12,15 @@ double-doubleにする場合はITV_FLGに1を設定
 コンパイルは  g++ func.cpp -O3 -DNDEBUG
 mpfrを使う場合はITV_FLGに2を設定
 kv::mpfr<PREC>のPRECは精度(bit)を設定
-単精度24bit倍精度53bit四倍精度113bit八倍精度237bit
 コンパイルは g++ func.cpp -lmpfr -O3 -DNDEBUG
 */
 #define ITV_FLG 2
 
 #if ITV_FLG == 2
-    //精度
+    // 精度
+    // 単精度24bit倍精度53bit四倍精度113bit八倍精度237bit
     #define PREC 53
-    //表示桁数
+    // 表示桁数
     #define DIGIT 17
     typedef kv::interval<kv::mpfr<PREC>> itv;
 #elif ITV_FLG == 1
@@ -34,12 +34,12 @@ T func(T x)
 {
     itv pi;
     pi = kv::constants<itv>::pi();
-     return (T)"1.0" / exp((T)"0.5" * x / pi);
+    return (T) "1.0" / exp((T) "0.5" * x / pi);
 }
 
 int main(void)
 {
-    itv x,res;
+    itv x, res;
     x = "0.1";
     res = func(x);
     std::cout.precision(DIGIT);
